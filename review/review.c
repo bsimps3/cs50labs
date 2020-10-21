58b96d1fd474a09bab8efc7f808e34d20b97a19c
Brian Simpson <bsimps3@gmail.com>
2020-10-21 09:43:04 -0500

    updated to include while loops

--- review/review.c
+++ review/review.c
@@ -4,16 +4,32 @@
 int main(void)
 {
 //this for loop prints "hello, world" 10 times
-//Can you change it so that it prints 100 times?
+//#1   Can you change it so that it prints 100 times?
     for (int i = 0; i < 10; i++) 
     {
         printf("hello, world\n");
     }
 
-    //Can you write a for loop which prints the numbers (each on a new line) from 50 to 0 in decreasing order
+    //#2   Can you write a for loop which prints the numbers (each on a new line) from 50 to 0 in decreasing order
 
 
-    //Can you write a for loop which prints only even numbers starting at 10 and going to 100
+    //#3    Can you write a for loop which prints only even numbers starting at 10 and going to 100
 
-    //Can you write a for loop which adds up all the numbers from 17 to 78?
+    //#4    Can you write a for loop which adds up all the numbers from 17 to 78?
+
+    //WHILE LOOPS
+    //#5   Can you write a while loop which counts up to 25 using the counter variable below?
+    int counter = 0;
+
+    //#6   Can you write a while loop which divides the variable below by 3 repeatedly AND counts how many divisions occur before you get to 0? Use the numberOfDivisions variable to keep track of that.
+    int number = 3078;
+    int numberOfDivisions = 0;
+
+    //#7  Can you write a while loop that prints the numbers from 50 to 0 (each on a new line) in decreasing order?
+    int numberToPrint = 50;  //use this in your loop
+
+    //#8  Can you write a write a while loop which adds up all the numbers from 17 to 78? Create whichever variables you need.
+
+    //#9  Create a while loop which prints only even numbers starting at 10 and going to 100. 
 }
+
